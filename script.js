d35aa78be2ca455f7ee8f5d96b500953e7a29de3
Luiz Felipe Monteiro <luizfelipemonteiro708@gmail.com>
2025-11-24 08:28:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert("Obrigado! Sua mensagem foi enviada com sucesso.");
 });
+
+function myFunction() {
+    document.querySelector(".main-container").classList.toggle("dark-theme");
+}
